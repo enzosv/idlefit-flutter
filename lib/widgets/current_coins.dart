4645d3765c0679080efc939df7db6b6aeeca0bc5
enzosv <vergara.enzo@gmail.com>
2025-03-11 15:15:01 +0800

    add text animation to current coins

--- lib/widgets/current_coins.dart
+++ lib/widgets/current_coins.dart
@@ -3,20 +3,69 @@ import 'package:provider/provider.dart';
 import '../services/game_state.dart';
 import '../util.dart';
 
-class CurrentCoins extends StatelessWidget {
+class CurrentCoins extends StatefulWidget {
   const CurrentCoins({super.key});
+
+  @override
+  State<CurrentCoins> createState() => _CurrentCoinsState();
+}
+
+class _CurrentCoinsState extends State<CurrentCoins>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+  double _previousCoins = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+
+    _scaleAnimation = TweenSequence<double>([
+      TweenSequenceItem(tween: Tween<double>(begin: 1.0, end: 1.3), weight: 1),
+      TweenSequenceItem(tween: Tween<double>(begin: 1.3, end: 1.0), weight: 1),
+    ]).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+  }
+
+  void _triggerAnimation() {
+    _animationController.forward(from: 0.0);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Consumer<GameState>(
       builder: (context, gameState, child) {
-        return Text(
-          toLettersNotation(gameState.coins.count),
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
+        // if (gameState.coins.count != _previousCoins) {
+        //   _previousCoins = gameState.coins.count;
+        //   _triggerAnimation();
+        // }
+
+        return AnimatedBuilder(
+          animation: _scaleAnimation,
+          builder: (context, child) {
+            return Transform.scale(
+              scale: _scaleAnimation.value,
+              child: Text(
+                toLettersNotation(gameState.coins.count),
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            );
+          },
         );
       },
     );
