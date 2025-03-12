62398426abd440d613436acf2a36ce3ec14163dc
enzosv <vergara.enzo@gmail.com>
2025-03-12 13:37:14 +0800

    use common card for generator upgrade

--- lib/widgets/generator_upgrade_card.dart
+++ lib/widgets/generator_upgrade_card.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:idlefit/models/coin_generator.dart';
 import 'package:idlefit/services/game_state.dart';
 import 'package:idlefit/util.dart';
-import 'package:idlefit/widgets/card_button.dart';
+import 'common_card.dart';
 
-class GeneratorUpgradeCard extends StatefulWidget {
+class GeneratorUpgradeCard extends StatelessWidget {
   final GameState gameState;
   final CoinGenerator generator;
 
@@ -15,132 +15,84 @@ class GeneratorUpgradeCard extends StatefulWidget {
   });
 
   @override
-  _GeneratorUpgradeCardState createState() => _GeneratorUpgradeCardState();
-}
-
-class _GeneratorUpgradeCardState extends State<GeneratorUpgradeCard> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final needsSpace = !widget.generator.isUnlocked;
-    final canAffordSpace =
-        widget.gameState.space.count >= widget.generator.upgradeUnlockCost;
-    final canAffordCoins =
-        widget.gameState.coins.count >= widget.generator.upgradeCost;
+    final needsSpace = !generator.isUnlocked;
+    final canAffordSpace = gameState.space.count >= generator.upgradeUnlockCost;
+    final canAffordCoins = gameState.coins.count >= generator.upgradeCost;
 
-    return Card(
-      margin: const EdgeInsets.only(bottom: 16),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    final additionalInfo = <Widget>[
+      Row(
+        children: [
+          Text(
+            'Current output: ${toLettersNotation(generator.output)} ',
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          Icon(Icons.monetization_on, color: Colors.amber, size: 16),
+          Text('/sec', style: Theme.of(context).textTheme.bodyMedium),
+        ],
+      ),
+    ];
+
+    if (generator.level < generator.maxLevel) {
+      additionalInfo.add(
+        Row(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  widget.generator.name,
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                Text(
-                  'Level: ${widget.generator.level}/${widget.generator.maxLevel}',
-                ),
-              ],
+            Text(
+              'Next level: ${toLettersNotation(generator.outputAtLevel(generator.level + 1))} ',
+              style: Theme.of(context).textTheme.bodyMedium,
             ),
-            Row(
-              children: [
-                Text(
-                  'Current output: ${toLettersNotation(widget.generator.output)} ',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Icon(Icons.monetization_on, color: Colors.amber, size: 16),
-                Text('/sec', style: Theme.of(context).textTheme.bodyMedium),
-              ],
-            ),
-            if (widget.generator.level < widget.generator.maxLevel)
-              Row(
-                children: [
-                  Text(
-                    'Next level: ${toLettersNotation(widget.generator.outputAtLevel(widget.generator.level + 1))} ',
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                  Icon(Icons.monetization_on, color: Colors.amber, size: 16),
-                  Text('/sec', style: Theme.of(context).textTheme.bodyMedium),
-                ],
-              ),
-            const SizedBox(height: 16),
-            if (needsSpace) ...[
-              Row(
-                children: [
-                  Text(
-                    'Cost: ${toLettersNotation(widget.generator.upgradeUnlockCost)} ',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: canAffordSpace ? Colors.green : Colors.red,
-                    ),
-                  ),
-                  Icon(
-                    Icons.space_dashboard,
-                    color: canAffordSpace ? Colors.green : Colors.red,
-                    size: 20,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 8),
-              Align(
-                alignment: Alignment.centerRight,
-                child: CardButton(
-                  onPressed:
-                      canAffordSpace
-                          ? () {
-                            widget.gameState.unlockGenerator(widget.generator);
-                          }
-                          : null,
-                  text: 'Unlock',
-                ),
-              ),
-            ] else ...[
-              if (widget.generator.level < widget.generator.maxLevel) ...[
-                Row(
-                  children: [
-                    Text(
-                      'Cost: ${toLettersNotation(widget.generator.upgradeCost)} ',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: canAffordCoins ? Colors.green : Colors.red,
-                      ),
-                    ),
-                    Icon(
-                      Icons.monetization_on,
-                      color: canAffordCoins ? Colors.green : Colors.red,
-                      size: 20,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 8),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: CardButton(
-                    onPressed:
-                        canAffordCoins
-                            ? () {
-                              widget.gameState.upgradeGenerator(
-                                widget.generator,
-                              );
-                            }
-                            : null,
-                    text: 'Upgrade',
-                  ),
-                ),
-              ],
-            ],
+            Icon(Icons.monetization_on, color: Colors.amber, size: 16),
+            Text('/sec', style: Theme.of(context).textTheme.bodyMedium),
           ],
         ),
-      ),
+      );
+    }
+
+    if (needsSpace) {
+      return CommonCard(
+        title: generator.name,
+        rightText: 'Level: ${generator.level}/${generator.maxLevel}',
+        description: generator.description,
+        additionalInfo: additionalInfo,
+        costText: 'Cost: ${toLettersNotation(generator.upgradeUnlockCost)}',
+        costColor: canAffordSpace ? Colors.green : Colors.red,
+        costIcon: Icon(
+          Icons.space_dashboard,
+          color: canAffordSpace ? Colors.green : Colors.red,
+          size: 20,
+        ),
+        buttonText: 'Unlock',
+        onButtonPressed:
+            canAffordSpace ? () => gameState.unlockGenerator(generator) : null,
+      );
+    }
+
+    if (generator.level < generator.maxLevel) {
+      return CommonCard(
+        title: generator.name,
+        rightText: 'Level: ${generator.level}/${generator.maxLevel}',
+        description: generator.description,
+        additionalInfo: additionalInfo,
+        costText: 'Cost: ${toLettersNotation(generator.upgradeCost)}',
+        costColor: canAffordCoins ? Colors.green : Colors.red,
+        costIcon: Icon(
+          Icons.monetization_on,
+          color: canAffordCoins ? Colors.green : Colors.red,
+          size: 20,
+        ),
+        buttonText: 'Upgrade',
+        onButtonPressed:
+            canAffordCoins ? () => gameState.upgradeGenerator(generator) : null,
+      );
+    }
+
+    return CommonCard(
+      title: generator.name,
+      rightText: 'Level: ${generator.level}/${generator.maxLevel}',
+      description: generator.description,
+      additionalInfo: additionalInfo,
+      buttonText: 'MAXED',
+      onButtonPressed: null,
     );
   }
 }
