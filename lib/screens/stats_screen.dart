6f84e6df63a8fe3e31a966b9962928ab6fb4ab4c
enzosv <vergara.enzo@gmail.com>
2025-03-12 16:44:44 +0800

    remove currency bar in stats screen

--- lib/screens/stats_screen.dart
+++ lib/screens/stats_screen.dart
@@ -1,28 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:idlefit/constants.dart';
 import 'package:idlefit/widgets/health_stats_card.dart';
 import 'package:idlefit/widgets/game_stats_card.dart';
-import 'package:provider/provider.dart';
-import '../services/game_state.dart';
-import '../widgets/common_widgets.dart';
-import '../util.dart';
 
 class StatsScreen extends StatelessWidget {
   const StatsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final gameState = Provider.of<GameState>(context);
     return SafeArea(
       top: false,
       child: Column(
         children: [
-          Container(
-            height: MediaQuery.paddingOf(context).top,
-            color: Constants.barColor,
-          ),
-          // Currency display
-          const CurrencyBar(),
+          Container(height: MediaQuery.paddingOf(context).top),
 
           // Stats cards
           Expanded(
