a66b6460c22b0aef7730bb1bf0c8d048c514303c
enzosv <vergara.enzo@gmail.com>
2025-03-12 13:37:25 +0800

    use common card for shop item

--- lib/widgets/shop_item_card.dart
+++ lib/widgets/shop_item_card.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:idlefit/widgets/card_button.dart';
 import 'package:provider/provider.dart';
 import '../services/game_state.dart';
 import '../models/shop_items.dart';
+import 'common_card.dart';
 
 class ShopItemCard extends StatelessWidget {
   final ShopItem item;
@@ -14,54 +14,29 @@ class ShopItemCard extends StatelessWidget {
     final gameState = Provider.of<GameState>(context);
     final isMaxLevel = item.level >= item.maxLevel;
 
-    return Card(
-      margin: const EdgeInsets.only(bottom: 16),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(item.name, style: Theme.of(context).textTheme.titleMedium),
-                Text('Level: ${item.level}/${item.maxLevel}'),
-              ],
-            ),
-            const SizedBox(height: 8),
-            Text(item.description),
-            if (item.level > 0)
-              Text('Current effect: ${item.currentEffectValue}'),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  isMaxLevel
-                      ? 'MAXED OUT'
-                      : 'Cost: ${item.currentCost} ${item.costUnit.name}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color:
-                        isMaxLevel
-                            ? Colors.grey
-                            : gameState.space.count >= item.currentCost
-                            ? Colors.green
-                            : Colors.red,
-                  ),
-                ),
-                CardButton(
-                  text: 'Upgrade',
-                  onPressed:
-                      (isMaxLevel || gameState.space.count < item.currentCost)
-                          ? null
-                          : () => gameState.upgradeShopItem(item),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+    return CommonCard(
+      title: item.name,
+      rightText: 'Level: ${item.level}/${item.maxLevel}',
+      description: item.description,
+      additionalInfo:
+          item.level > 0
+              ? [Text('Current effect: ${item.currentEffectValue}')]
+              : [],
+      costText:
+          isMaxLevel
+              ? 'MAXED OUT'
+              : 'Cost: ${item.currentCost} ${item.costUnit.name}',
+      costColor:
+          isMaxLevel
+              ? Colors.grey
+              : gameState.space.count >= item.currentCost
+              ? Colors.green
+              : Colors.red,
+      buttonText: 'Upgrade',
+      onButtonPressed:
+          (isMaxLevel || gameState.space.count < item.currentCost)
+              ? null
+              : () => gameState.upgradeShopItem(item),
     );
   }
 }
