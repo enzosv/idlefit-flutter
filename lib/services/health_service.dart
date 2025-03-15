38422aaba6d5f196a504cb5337e750c5651b700c
enzosv <vergara.enzo@gmail.com>
2025-03-16 00:44:27 +0800

    do not estimate exercise time

--- lib/services/health_service.dart
+++ lib/services/health_service.dart
@@ -5,7 +5,6 @@ import 'package:idlefit/models/health_data_entry.dart';
 import 'package:idlefit/models/health_data_repo.dart';
 import 'package:idlefit/providers/game_state_provider.dart';
 import 'package:idlefit/services/object_box.dart';
-import 'dart:math';
 
 class HealthService {
   final Health health = Health();
@@ -40,62 +39,6 @@ class HealthService {
       debugPrint('Error requesting health authorization: $e');
       return false;
     }
-  }
-
-  // check calories burned time
-  // if lots burned in short time, count that as exercise time
-  // any minute over 120bpm?
-  double estimateExerciseTime(List<HealthDataPoint> healthData) {
-    final data =
-        healthData
-            .where((data) => data.type == HealthDataType.ACTIVE_ENERGY_BURNED)
-            .toList();
-
-    if (data.isEmpty) {
-      return 0;
-    }
-
-    Map<String, double> grouped = {};
-    for (final entry in data) {
-      final value = (entry.value as NumericHealthValue).numericValue.toDouble();
-
-      final start = entry.dateFrom.millisecondsSinceEpoch;
-      final end = entry.dateTo.millisecondsSinceEpoch;
-      int duration = end - start;
-      final bpm = estimateBPM(value, duration);
-      duration = max(1, duration);
-      if (bpm > 120) {
-        grouped[entry.sourceName] = (grouped[entry.sourceName] ?? 0) + duration;
-      }
-    }
-    String? bestSource =
-        grouped.keys.isEmpty
-            ? null
-            : grouped.keys.reduce((a, b) => grouped[a]! > grouped[b]! ? a : b);
-    return bestSource != null ? grouped[bestSource]! / 1000 / 60 : 0;
-  }
-
-  // use METs (Metabolic Equivalent of Task) or caloric expenditure formulas.
-  double estimateBPM(
-    double caloriesBurned,
-    int durationMs, {
-    double weightKg = 70,
-    bool isMale = true,
-  }) {
-    if (durationMs <= 0) return 0; // Avoid division by zero
-    double durationMinutes = durationMs / 60000.0;
-    double k =
-        isMale ? 0.6309 : 0.4472; // Different coefficient for males & females
-
-    // Estimate heart rate using rearranged formula
-
-    // Calories per minute= ((heartrate * weight * K)/1000)*5
-    // cpm = 5*(hr*w*k)/1000
-    // (1000*cpm)/5 = hr*w*k
-    // 1000*cpm/5/w/k=hr
-    print("cpm: ${caloriesBurned * 1000 / durationMinutes}");
-    return (1000 * caloriesBurned) / (durationMinutes * 5 * weightKg * k);
-    // return ((caloriesBurned / (durationMinutes * 5)) * 1000) / (weightKg * k);
   }
 
   Future<List<HealthDataEntry>> queryHealthEntries(
