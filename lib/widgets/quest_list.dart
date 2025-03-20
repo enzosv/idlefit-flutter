742cad3062040169bcb1c0ffcd97a5c9f6a5521f
enzosv <vergara.enzo@gmail.com>
2025-03-20 14:58:35 +0800

    convert questlist to consumerwidget

--- lib/widgets/quest_list.dart
+++ lib/widgets/quest_list.dart
@@ -6,40 +6,27 @@ import 'package:idlefit/models/quest_stats.dart';
 import 'package:idlefit/providers/currency_provider.dart';
 import 'package:idlefit/widgets/quest_card.dart';
 
-class QuestList extends ConsumerStatefulWidget {
+class QuestList extends ConsumerWidget {
   final QuestType questType;
   const QuestList({super.key, required this.questType});
 
-  @override
-  ConsumerState<QuestList> createState() => _QuestListState();
-}
-
-class _QuestListState extends ConsumerState<QuestList> {
-  List<Quest> quests = [];
-  late QuestRepository _questRepo;
-
-  @override
-  void initState() {
-    super.initState();
-    _questRepo = ref.read(questRepositoryProvider);
-  }
-
-  void onClaim(Quest quest) {
-    // assuming only coins and spaces are rewarded
+  void _onClaim(WidgetRef ref, Quest quest) {
     final currencyProvider =
         quest.rewardCurrency == CurrencyType.coin
             ? ref.read(coinProvider.notifier)
             : ref.read(spaceProvider.notifier);
-    _questRepo.claimQuest(
-      quest,
-      ref.read(questStatsRepositoryProvider),
-      currencyProvider,
-    );
-    setState(() {});
+
+    ref
+        .read(questRepositoryProvider)
+        .claimQuest(
+          quest,
+          ref.read(questStatsRepositoryProvider),
+          currencyProvider,
+        );
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(16),
@@ -47,23 +34,18 @@ class _QuestListState extends ConsumerState<QuestList> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              widget.questType.displayName,
+              questType.displayName,
               style: Theme.of(context).textTheme.headlineSmall,
             ),
             const Divider(),
             FutureBuilder<List<Quest>>(
-              future: () async {
-                final List<Quest> quests = await _questRepo.getQuests(
-                  widget.questType,
-                );
-                return quests;
-              }(),
+              future: ref.read(questRepositoryProvider).getQuests(questType),
               builder: (context, snapshot) {
                 if (!snapshot.hasData) {
                   return const Center(child: CircularProgressIndicator());
                 }
-                if (snapshot.data == null) {
-                  return const Center(child: Text('No quests found'));
+                if (snapshot.data == null || snapshot.data!.isEmpty) {
+                  return const Center(child: Text('No quests available'));
                 }
                 final quests = snapshot.data!;
                 return Column(
@@ -71,9 +53,9 @@ class _QuestListState extends ConsumerState<QuestList> {
                       quests.map((quest) {
                         return QuestCard(
                           quest: quest,
-                          onClaim: () => onClaim(quest),
+                          onClaim: () => _onClaim(ref, quest),
                         );
-                      }).toList(), // Convert Iterable to List
+                      }).toList(),
                 );
               },
             ),
