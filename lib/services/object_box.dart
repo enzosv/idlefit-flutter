4192a3b432fc75e4521b055274d4e7d56494aec8
enzosv <vergara.enzo@gmail.com>
2025-03-24 23:03:41 +0800

    cleanup

--- lib/services/object_box.dart
+++ lib/services/object_box.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 import '../objectbox.g.dart'; // created by `flutter pub run build_runner build`
