ea03938d7e42f398c85033391e289937b24654fe
enzosv <vergara.enzo@gmail.com>
2025-03-29 12:16:18 +0800

    fix analyze issue

--- lib/widgets/health_stats_card.dart
+++ lib/widgets/health_stats_card.dart
@@ -6,25 +6,25 @@ import 'package:idlefit/helpers/util.dart';
 import 'package:idlefit/widgets/card_button.dart';
 import 'package:idlefit/providers/providers.dart';
 
-class _HealthStatsTile extends StatelessWidget {
+final _healthStatsProvider =
+    FutureProvider.family<(double, double), (QuestAction, QuestUnit)>((
+      ref,
+      params,
+    ) async {
+      final (action, unit) = params;
+      final repository = ref.read(questStatsRepositoryProvider);
+      final today = repository.getProgress(action, unit, todayTimestamp);
+      final total = repository.getTotalProgress(action, unit);
+      return (today, total).wait;
+    });
+
+class _HealthStatsTile extends ConsumerWidget {
   final QuestAction action;
   final QuestUnit unit;
   final IconData icon;
   final String title;
   final Color? iconColor;
   final QuestStatsRepository repository;
-
-  final _healthStatsProvider =
-      FutureProvider.family<(double, double), (QuestAction, QuestUnit)>((
-        ref,
-        params,
-      ) async {
-        final (action, unit) = params;
-        final repository = ref.read(questStatsRepositoryProvider);
-        final today = repository.getProgress(action, unit, todayTimestamp);
-        final total = repository.getTotalProgress(action, unit);
-        return (today, total).wait;
-      });
 
   const _HealthStatsTile({
     required this.action,
@@ -36,7 +36,7 @@ class _HealthStatsTile extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final statsAsync = ref.watch(_healthStatsProvider((action, unit)));
     return statsAsync.when(
       loading:
@@ -52,11 +52,11 @@ class _HealthStatsTile extends StatelessWidget {
             subtitle: const Text('Error loading'),
           ),
       data:
-          (today, total) => ListTile(
+          (data) => ListTile(
             leading: Icon(icon, color: iconColor, size: 28),
             title: Text(title),
-            subtitle: Text('Today: ${toLettersNotation(today)}'),
-            trailing: Text('Total: ${toLettersNotation(total)}'),
+            subtitle: Text('Today: ${toLettersNotation(data.$1)}'),
+            trailing: Text('Total: ${toLettersNotation(data.$2)}'),
           ),
     );
   }
