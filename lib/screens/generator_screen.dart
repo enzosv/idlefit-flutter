07ab969d4b8c7fe5332971c433e24f958e494b02
enzosv <vergara.enzo@gmail.com>
2025-03-29 13:01:52 +0800

    adjust offset on new generator affordable

--- lib/screens/generator_screen.dart
+++ lib/screens/generator_screen.dart
@@ -1,40 +1,62 @@
-import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:idlefit/models/coin_generator.dart';
 import 'package:idlefit/widgets/generator_card.dart';
 import 'package:idlefit/providers/providers.dart';
 
-class FlameBackground extends FlameGame {
+class GeneratorsScreen extends StatefulWidget {
+  const GeneratorsScreen({super.key});
+
   @override
-  Future<void> onLoad() async {
-    // Add background animations here
-  }
+  State<GeneratorsScreen> createState() => _GeneratorsScreenState();
 }
 
-class GeneratorsScreen extends StatelessWidget {
-  const GeneratorsScreen({super.key});
+class _GeneratorsScreenState extends State<GeneratorsScreen> {
+  final ScrollController _scrollController = ScrollController();
+  CoinGenerator? _previousTopGenerator;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        // GameWidget(game: FlameBackground()),
-
-        // Generators list
         Expanded(
           child: Consumer(
             builder: (context, ref, child) {
               final coinGenerators = ref.watch(generatorProvider);
               final coins = ref.watch(coinProvider);
 
-              // Filter affordable generators and sort by price
+              // Get affordable generators and sort them
               final affordableGenerators =
                   coinGenerators
                       .where((generator) => generator.cost <= coins.max)
                       .toList()
                     ..sort((a, b) => b.tier.compareTo(a.tier));
-              // TODO: new generator should require scroll up
+
+              // Identify the new top generator
+              final newTopGenerator =
+                  affordableGenerators.isNotEmpty
+                      ? affordableGenerators.first
+                      : null;
+
+              // If the top generator has changed, adjust the scroll position
+              if (_previousTopGenerator != null &&
+                  newTopGenerator != null &&
+                  _previousTopGenerator != newTopGenerator &&
+                  _scrollController.hasClients) {
+                WidgetsBinding.instance.addPostFrameCallback((_) {
+                  // Scroll down slightly to keep the second generator at the top
+                  if (_scrollController.hasClients) {
+                    _scrollController.jumpTo(
+                      _scrollController.position.minScrollExtent + 100,
+                    );
+                  }
+                });
+                // Update previous top generator
+                _previousTopGenerator = newTopGenerator;
+              }
+
               return ListView.builder(
+                controller: _scrollController,
                 padding: const EdgeInsets.all(16),
                 itemCount: affordableGenerators.length,
                 itemBuilder: (context, index) {
